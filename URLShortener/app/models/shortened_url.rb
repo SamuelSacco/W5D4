366c6dad82f1cb87156ef38bd67d07ae60f27480
Samuel Sacco <SamLSacco@aol.com>
2021-05-20 17:22:57 -0400

    class method  testing

--- URLShortener/app/models/shortened_url.rb
+++ URLShortener/app/models/shortened_url.rb
@@ -9,6 +9,26 @@
 #  created_at :datetime         not null
 #  updated_at :datetime         not null
 #
+require 'securerandom'
+
 class ShortenedUrl < ApplicationRecord
+  include SecureRandom
   validates :long_url, presence: true
+
+  def self.random_code
+    flag = false
+    until flag 
+      random_string = SecureRandom::urlsafe_base64
+      if !self.exists?(random_string)
+        flag = true
+      end
+    end
+
+    random_string
+  end
+
+  def self.create!(user_object, long_url)
+    ShortenedUrl.create(long_url: long_url, short_url: random_code, 
+    user_id: user_object.id)
+  end
 end
